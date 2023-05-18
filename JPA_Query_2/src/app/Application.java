276cf3b73c3cf38ab2cf1050c94a830f975b0aca
AndreaBar94 <andreabarocchi@gmail.com>
2023-05-18 17:05:19 +0200

    edit comments, queries to test

--- JPA_Query_2/src/app/Application.java
+++ JPA_Query_2/src/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
@@ -47,43 +48,46 @@ public class Application {
 		Evento cena2 = new Evento("Pranzo", LocalDate.now(), "Pranzo di beneficenza", TipoEvento.PUBBLICO, 50, loc1, new HashSet<>());
 		Evento concerto1 = new Concerto("Queen", LocalDate.now(), "Concerto bello", TipoEvento.PUBBLICO, 50, loc1, new HashSet<>(), Genere.ROCK, false);
 		Evento partita1 = new Partita_Di_Calcio("Derby", LocalDate.now(), "Derby Roma - Lazio", TipoEvento.PUBBLICO, 80000, loc1,  new HashSet<>(), "Roma", "Lazio", "Roma", 1, 1);
+		Evento partita2 = new Partita_Di_Calcio("Derby", LocalDate.now(), "Derby Roma - Lazio", TipoEvento.PUBBLICO, 80000, loc1,  new HashSet<>(), "Roma", "Lazio", "Roma", 1, 1);
+		Evento partita3 = new Partita_Di_Calcio("Derby", LocalDate.now(), "Derby Roma - Lazio", TipoEvento.PUBBLICO, 80000, loc1,  new HashSet<>(), "Roma", "Lazio", "Roma", 1, 1);
 		Evento gara1 = new Gara_Di_Atletica("Nazionali Calisthenics", LocalDate.now(), "Finali di Calisthenics", TipoEvento.PUBBLICO, 100, loc1,  new HashSet<>(), new HashSet<>(), aldo);
 		
-		pDAO.save(maria);
-		pDAO.save(aldo);
-		lDAO.save(loc1);
+//		pDAO.save(maria);
+//		pDAO.save(aldo);
+//		lDAO.save(loc1);
 		
 		Partecipazione p1 = new Partecipazione(aldo, concerto1, Stato.CONFERMATA);
 		Partecipazione p2 = new Partecipazione(aldo, cena2, Stato.CONFERMATA);
 		
 		
 		Set<Partecipazione> lp1 = new HashSet<>();
-//	
-		cena1.getPartecipazioni().add(p1);
-		cena2.getPartecipazioni().add(p2);
-		concerto1.getPartecipazioni().add(p1);
-		partita1.getPartecipazioni().add(p2);
-		gara1.getPartecipazioni().add(p1);
 		
 		
-		p1.setEvento(concerto1);
-		p1.setPersona(maria);
-		
-		p2.setEvento(cena2);
-		p2.setPersona(aldo);
+//		cena1.getPartecipazioni().add(p1);
+//		cena2.getPartecipazioni().add(p2);
+//		concerto1.getPartecipazioni().add(p1);
+//		partita1.getPartecipazioni().add(p2);
+//		gara1.getPartecipazioni().add(p1);
+//		
+//		
+//		p1.setEvento(concerto1);
+//		p1.setPersona(maria);
+//		
+//		p2.setEvento(cena2);
+//		p2.setPersona(aldo);
 
 		
 		
-//		logger.info("GUARDA QUI: "+ p1.getEvento());
+//		logger.info("GUARDA QUI: "+ partita1.getPartiteVinteInCasa());
 //		sd.delete(47);
 //		sd.delete(48);
 		em.getTransaction().begin();
-		em.persist(concerto1);
-		em.persist(partita1);
-		em.persist(gara1);
-		em.persist(cena2);
-		em.persist(p1);
-		em.persist(p2);
+//		em.persist(concerto1);
+//		em.persist(partita1);
+//		em.persist(gara1);
+//		em.persist(cena2);
+//		em.persist(p1);
+//		em.persist(p2);
 		em.getTransaction().commit();
 
 		sd.refresh(4);
